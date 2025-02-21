db42334d57f6e4bc5a8aa78b30db0757e4450245
boubouthiamniang <boubouniang@yahoo.fr>
2025-02-21 21:35:57 +0100

    Update publisherConfirmsAsync

--- src/main/java/com/messaging/rabbitmq/productVariants/PublisherConfirms/PublisherConfirmsAsync.java
+++ src/main/java/com/messaging/rabbitmq/productVariants/PublisherConfirms/PublisherConfirmsAsync.java
@@ -15,26 +15,18 @@ public class PublisherConfirmsAsync {
 
     static final int MESSAGE_COUNT = 50000;
 
-    static Connection createConnection() throws Exception {
-        ConnectionFactory cf = new ConnectionFactory();
-        cf.setHost("localhost");
-        cf.setUsername("guest");
-        cf.setPassword("guest");
-        return cf.newConnection();
-    }
-
     public static void main(String[] args) throws Exception {
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
   
-        try (Connection connection = createConnection()) {
-            Channel ch = connection.createChannel();
+        try (Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
 
             String queue = UUID.randomUUID().toString();
-            ch.queueDeclare(queue, false, false, true, null);
+            channel.queueDeclare(queue, false, false, true, null);
 
-            ch.confirmSelect();
+            channel.confirmSelect();
 
             ConcurrentNavigableMap<Long, String> outstandingConfirms = new ConcurrentSkipListMap<>();
 
@@ -49,7 +41,7 @@ public class PublisherConfirmsAsync {
                 }
             };
 
-            ch.addConfirmListener(cleanOutstandingConfirms, (sequenceNumber, multiple) -> {
+            channel.addConfirmListener(cleanOutstandingConfirms, (sequenceNumber, multiple) -> {
                 String body = outstandingConfirms.get(sequenceNumber);
                 System.err.format(
                         "Message with body %s has been nack-ed. Sequence number: %d, multiple: %b%n",
@@ -61,106 +53,8 @@ public class PublisherConfirmsAsync {
             long start = System.nanoTime();
             for (int i = 0; i < MESSAGE_COUNT; i++) {
                 String body = String.valueOf(i);
-                outstandingConfirms.put(ch.getNextPublishSeqNo(), body);
-                ch.basicPublish("", queue, null, body.getBytes());
-            }
-
-            if (!waitUntil(Duration.ofSeconds(60), () -> outstandingConfirms.isEmpty())) {
-                throw new IllegalStateException("All messages could not be confirmed in 60 seconds");
-            }
-
-            long end = System.nanoTime();
-            System.out.format("Published %,d messages and handled confirms asynchronously in %,d ms%n", MESSAGE_COUNT, Duration.ofNanos(end - start).toMillis());
-        }
-    }
-
-    static void publishMessagesIndividually() throws Exception {
-        try (Connection connection = createConnection()) {
-            Channel ch = connection.createChannel();
-
-            String queue = UUID.randomUUID().toString();
-            ch.queueDeclare(queue, false, false, true, null);
-
-            ch.confirmSelect();
-            long start = System.nanoTime();
-            for (int i = 0; i < MESSAGE_COUNT; i++) {
-                String body = String.valueOf(i);
-                ch.basicPublish("", queue, null, body.getBytes());
-                ch.waitForConfirmsOrDie(5000);
-            }
-            long end = System.nanoTime();
-            System.out.format("Published %,d messages individually in %,d ms%n", MESSAGE_COUNT, Duration.ofNanos(end - start).toMillis());
-        }
-    }
-
-    static void publishMessagesInBatch() throws Exception {
-        try (Connection connection = createConnection()) {
-            Channel ch = connection.createChannel();
-
-            String queue = UUID.randomUUID().toString();
-            ch.queueDeclare(queue, false, false, true, null);
-
-            ch.confirmSelect();
-
-            int batchSize = 100;
-            int outstandingMessageCount = 0;
-
-            long start = System.nanoTime();
-            for (int i = 0; i < MESSAGE_COUNT; i++) {
-                String body = String.valueOf(i);
-                ch.basicPublish("", queue, null, body.getBytes());
-                outstandingMessageCount++;
-
-                if (outstandingMessageCount == batchSize) {
-                    ch.waitForConfirmsOrDie(5_000);
-                    outstandingMessageCount = 0;
-                }
-            }
-
-            if (outstandingMessageCount > 0) {
-                ch.waitForConfirmsOrDie(5_000);
-            }
-            long end = System.nanoTime();
-            System.out.format("Published %,d messages in batch in %,d ms%n", MESSAGE_COUNT, Duration.ofNanos(end - start).toMillis());
-        }
-    }
-
-    static void handlePublishConfirmsAsynchronously() throws Exception {
-        try (Connection connection = createConnection()) {
-            Channel ch = connection.createChannel();
-
-            String queue = UUID.randomUUID().toString();
-            ch.queueDeclare(queue, false, false, true, null);
-
-            ch.confirmSelect();
-
-            ConcurrentNavigableMap<Long, String> outstandingConfirms = new ConcurrentSkipListMap<>();
-
-            ConfirmCallback cleanOutstandingConfirms = (sequenceNumber, multiple) -> {
-                if (multiple) {
-                    ConcurrentNavigableMap<Long, String> confirmed = outstandingConfirms.headMap(
-                            sequenceNumber, true
-                    );
-                    confirmed.clear();
-                } else {
-                    outstandingConfirms.remove(sequenceNumber);
-                }
-            };
-
-            ch.addConfirmListener(cleanOutstandingConfirms, (sequenceNumber, multiple) -> {
-                String body = outstandingConfirms.get(sequenceNumber);
-                System.err.format(
-                        "Message with body %s has been nack-ed. Sequence number: %d, multiple: %b%n",
-                        body, sequenceNumber, multiple
-                );
-                cleanOutstandingConfirms.handle(sequenceNumber, multiple);
-            });
-
-            long start = System.nanoTime();
-            for (int i = 0; i < MESSAGE_COUNT; i++) {
-                String body = String.valueOf(i);
-                outstandingConfirms.put(ch.getNextPublishSeqNo(), body);
-                ch.basicPublish("", queue, null, body.getBytes());
+                outstandingConfirms.put(channel.getNextPublishSeqNo(), body);
+                channel.basicPublish("", queue, null, body.getBytes());
             }
 
             if (!waitUntil(Duration.ofSeconds(60), () -> outstandingConfirms.isEmpty())) {
